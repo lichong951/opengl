fcdddf8226c5ef20e45b09a70c156d4f3691429f
lichong <lichong@lichongdeMacBook-Pro.local>
2017-07-12 20:56:53 +0800

    【Bug】运行异常原因未明；Sample8_6:足球碳结构

---
+++ sample8/src/main/java/top/lc951/sample8/_6/Constant.java
@@ -0,0 +1,16 @@
+package top.lc951.sample8._6;
+
+/**
+ * Created by lichong on 2017/7/12.
+ */
+
+class Constant {
+    //边长
+    public static final float LENGTH = 0.8f;
+    //球半径
+    public static final float BALL_R=0.5f;
+    //圆柱半径
+    public static final float R=0.05f;
+    //切分角度
+    public static final float ANGLE_SPAN=18;
+}
