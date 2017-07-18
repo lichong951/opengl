e7be951eeb6d48325080fa60ee596ab6cac47d58
lichong <lichong@lichongdeMacBook-Pro.local>
2017-07-18 10:15:11 +0800

    Sample14_1:砖块着色器

---
+++ sample14/src/main/java/top/lc951/sample14/MainActivity.java
@@ -0,0 +1,19 @@
+package top.lc951.sample14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sample14_1(View view){
+        Sample14_1_Activity.actionActivity(this);
+
+    }
+}
---
+++ sample14/src/main/java/top/lc951/sample14/_1/Constant_1.java
@@ -0,0 +1,14 @@
+package top.lc951.sample14._1;
+
+/**
+ * Created by lichong on 2017/7/18.
+ *
+ * @ Email lichongmac@163.com
+ */
+
+ class Constant_1 {
+    //单位尺寸
+    public static final float UNIT_SIZE=1f;
+    //计算GLSurfaceView的宽高比
+    public static float ratio;
+}
