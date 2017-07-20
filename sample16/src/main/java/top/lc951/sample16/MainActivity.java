44756d44031d60b4282d32c943f964e3b1f59820
lichong <lichong@lichongdeMacBook-Pro.local>
2017-07-20 16:45:40 +0800

    Sample16_1:AABB包围盒

---
+++ sample16/src/main/java/top/lc951/sample16/MainActivity.java
@@ -0,0 +1,18 @@
+package top.lc951.sample16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sample16_1(View view){
+        Sample16_1_Activity.actionActivity(this);
+    }
+}
---
+++ sample16/src/main/java/top/lc951/sample16/Sample16_1_Activity.java
@@ -0,0 +1,46 @@
+package top.lc951.sample16;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import top.lc951.sample16._1.MySurfaceView_1;
+
+public class Sample16_1_Activity extends AppCompatActivity {
+    private MySurfaceView_1 mGLSurfaceView;
+    public static void actionActivity(Context context){
+        context.startActivity(new Intent(context, Sample16_1_Activity.class));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置为全屏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //设置为横屏模式
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //初始化GLSurfaceView
+        mGLSurfaceView = new MySurfaceView_1(this);
+        setContentView(mGLSurfaceView);
+        mGLSurfaceView.requestFocus();//获取焦点
+        mGLSurfaceView.setFocusableInTouchMode(true);//设置为可触控
+    }
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+}
