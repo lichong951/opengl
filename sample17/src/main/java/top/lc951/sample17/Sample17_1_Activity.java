e6d7e663bb9803ad07dc1e1c525ff801cd69b402
lichong <lichong@lichongdeMacBook-Pro.local>
2017-07-21 17:54:10 +0800

    Sample17_1:箱子相互碰撞

---
+++ sample17/src/main/java/top/lc951/sample17/Sample17_1_Activity.java
@@ -0,0 +1,58 @@
+package top.lc951.sample17;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import top.lc951.sample17._1.MySurfaceView;
+
+public class Sample17_1_Activity extends AppCompatActivity {
+    private MySurfaceView mGLSurfaceView;
+    public static void actionActivity(Context context){
+        context.startActivity(new Intent(context, Sample17_1_Activity.class));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉标头
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//强制横屏
+
+        mGLSurfaceView = new MySurfaceView(this);
+        mGLSurfaceView.requestFocus();//获取焦点
+        mGLSurfaceView.setFocusableInTouchMode(true);//设置为可触控
+
+        setContentView(mGLSurfaceView);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent e)
+    {
+        if(keyCode==4)
+        {
+            System.exit(0);
+        }
+        return false;
+    }
+}
