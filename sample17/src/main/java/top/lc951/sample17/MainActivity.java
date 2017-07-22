1c9fed75dcf4ba360105e147f4c50f4f57f27627
lichong <lichong@luobotec.com>
2017-07-22 11:40:39 +0800

    Sample17_4:任意物体碰撞

--- sample17/src/main/java/top/lc951/sample17/MainActivity.java
+++ sample17/src/main/java/top/lc951/sample17/MainActivity.java
@@ -21,5 +21,8 @@ public class MainActivity extends AppCompatActivity {
     public void sample17_3(View view){
         Sample17_3_Activity.actionActivity(this);
     }
+    public void sample17_4(View view){
+        Sample17_4_Activity.actionActivity(this);
+    }
 
 }
---
+++ sample17/src/main/java/top/lc951/sample17/_4/BNThing.java
@@ -0,0 +1,13 @@
+package top.lc951.sample17._4;
+
+/**
+ * @author lichong_951@163.com
+ * @version V1.0
+ * @Description: TODO
+ * @date 2017/7/22  10:27
+ * @powered by lichong
+ */
+
+abstract class BNThing {
+    public abstract void drawSelf();
+}
