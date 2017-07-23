bc377c01cbf053f728c6e82cea36b24d656051d8
lichong <lichong@7moor.com>
2017-07-23 23:03:18 +0800

    Sample18_2:磁场感应器;Sample18_3:光线传感器

--- sample18/src/main/java/top/lc951/myapplication/MainActivity.java
+++ sample18/src/main/java/top/lc951/myapplication/MainActivity.java
@@ -15,4 +15,12 @@ public class MainActivity extends AppCompatActivity {
     public void sample18_1(View view){
         Sample18_1_Activity.actionActivity(this);
     }
+     public void sample18_2(View view){
+        Sample18_2_Activity.actionActivity(this);
+    }
+    public void sample18_3(View view){
+        Sample18_3_Activity.actionActivity(this);
+    }
+
+
 }
