78827bcc1430c4953082929bc104d74822b37ca0
lichong <lichong@lichongdeMacBook-Pro.local>
2017-07-24 11:14:36 +0800

    Sample19_1:3D拾取技术

---
+++ sample19/src/main/java/top/lc951/sample19/MainActivity.java
@@ -0,0 +1,18 @@
+package top.lc951.sample19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sample19_1(View view){
+        Sample19_1_Activity.actionActivity(this);
+    }
+}
---
+++ sample19/src/main/java/top/lc951/sample19/_1/TouchableObject.java
@@ -0,0 +1,36 @@
+package top.lc951.sample19._1;
+
+/**
+ * Created by lichong on 2017/7/24.
+ *
+ * @ Email lichongmac@163.com
+ */
+
+abstract class TouchableObject {
+    AABB3 preBox;//仿射变换之前的包围盒
+    float[] m = new float[16];//仿射变换的矩阵
+    //顶点颜色
+    float[] color=new float[]{1,1,1,1};
+    float size = 1.5f;;//放大的尺寸
+    //获得中心点位置和长宽高的方法
+    public AABB3 getCurrBox(){
+        return preBox.setToTransformedBox(m);//获取变换后的包围盒
+
+    }
+    //触控后的动作，根据需要要做相应改动
+    public void changeOnTouch(boolean flag){
+        if (flag) {
+            color = new float[] { 0, 1, 0, 1 };
+            size = 3f;
+        } else {
+            color = new float[] { 1, 1, 1, 1 };
+            size = 1.5f;
+        }
+    }
+    //复制变换矩阵
+    public void copyM(){
+        for(int i=0;i<16;i++){
+            m[i]=MatrixState.getMMatrix()[i];
+        }
+    }
+}
